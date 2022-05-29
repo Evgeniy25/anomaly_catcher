8c5b0f1cc5ac98477c1ee3a17d196ac2394ca2d3
Evgeny Prokopets <prokopetz.e@gmail.com>
2022-05-29 05:19:23 +0300

    upd v2

--- app/config.py
+++ app/config.py
@@ -1,4 +1,3 @@
-from sqlite3 import DatabaseError
 import os
 
 from starlette.config import Config
@@ -6,7 +5,7 @@ from starlette.datastructures import CommaSeparatedStrings, Secret
 
 dir_path=os.path.dirname(os.path.realpath(__file__))
 root_dir=dir_path[:-3]
-print(dir_path)
 config=Config(f'{root_dir}.env')
 DATABASE_URL = f'sqlite:///{root_dir}'+config('DB_NAME', cast=str)
 
+SECRET_KEY=config('SECRET_KEY', cast=Secret)
